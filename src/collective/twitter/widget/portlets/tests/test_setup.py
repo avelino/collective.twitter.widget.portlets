2d9f8b76cb8e19e96d739eb787cdd1cc2cb828c2
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-11-08 12:22:22 -0200

    Test add setup, lib is instaled

---
+++ src/collective/twitter/widget/portlets/tests/test_setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
+from plone.registry.interfaces import IRegistry
+
+from zope.component import getUtility
+
+from collective.facebook.portlets.config import PROJECTNAME
+from collective.facebook.portlets.testing import INTEGRATION_TESTING
+
+
+class InstallTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_installed(self):
+        qi = getattr(self.portal, 'portal_quickinstaller')
+        self.assertTrue(qi.isProductInstalled(PROJECTNAME))
+
+
+class UninstallTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.qi = self.portal['portal_quickinstaller']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.qi.uninstallProducts(products=[PROJECTNAME])
+
+    def test_uninstalled(self):
+        self.assertFalse(self.qi.isProductInstalled(PROJECTNAME))
+
